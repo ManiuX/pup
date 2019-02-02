4f4288f69d5e019fb36f301b3145a4a0b238345a
ManiuX <hy530k@163.com>
2019-02-02 14:56:26 +0800

    test the topo performance

--- example.js
+++ example.js
@@ -1,10 +1,20 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({headless:false,slowMo:1250});
     const page = await browser.newPage();
-    await page.goto('https://www.baidu.com');
+    await page.goto('http://www.hightopo.com/guide/guide/core/beginners/examples/example_editor.html');
     await page.screenshot({path: 'example.png'});
+
+    // await page.click('a')；
+
+    await page.mouse.click(140,14);
+
+    for(let i=0;i<1000;i++){
+        await page.mouse.click(162,148+i);
+    }
+
+
 
     await browser.close();
 })();
